bb28681d000d226d7b5e0d4ab6915cb0f61ddf48
Ben Parisot <me@benparisot.com>
2019-05-15 17:42:08 -0700

    get color changer to work with display font

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -2,10 +2,15 @@ import React, { PureComponent } from 'react';
 
 export default class TextFormatter extends PureComponent {
     state = {
-      text: ''
+      text: '',
+      color: 'black'
     }
     handleTextChange = ({ target }) => {
       this.setState({ text: target.value });
+    }
+
+    handleColorChange = ({ target }) => {
+      this.setState({ color: target.value });
     }
 
     render() {
@@ -16,8 +21,9 @@ export default class TextFormatter extends PureComponent {
           <>
             <form>
               <input onChange={this.handleTextChange}></input>
+              <input type="color" onChange={this.handleColorChange}></input>
             </form>
-            <p>{text}</p>  
+            <p style={{ color:this.state.color }}>{text}</p>  
         </> 
       );
     }
